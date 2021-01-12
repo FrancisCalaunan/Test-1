fd1ae9f70716236887a4871e69ab677d45333fdd
Francis110258 <110258@eesd.org>
2021-01-12 00:11:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a comment, comments are run by the compiler
+# Output: Information that the computer prints out, what you see printed on the right hand side
+
+print("Hello World")
+
+# Name, grade, favorite book, and favorite superhero
+
+print("My name is Francis Calaunan")
+print("I am in 7th grade")
+print("My favorite book is The Lost Hero")
+print("My favorite superhero is spiderman")
+
+print("Hello")
+print("World")
+
+# Concatenation: add two strings together using '+'
+
+print("Hello"+"World")
+
+print("Hi.")
